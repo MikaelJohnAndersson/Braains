65b89dafe107a71fcf0b4318d55490ccb7013926
Mikael John Andersson <mickepunktjohn@gmail.com>
2018-01-15 09:49:18 +0100

    Added  animation to menutext

--- core/src/com/mygdx3/game/BrainsMenu.java
+++ core/src/com/mygdx3/game/BrainsMenu.java
@@ -9,6 +9,8 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
+import com.badlogic.gdx.scenes.scene2d.actions.RepeatAction;
 
 public class BrainsMenu implements Screen{
 
@@ -17,6 +19,8 @@ public class BrainsMenu implements Screen{
     private Texture background;
     private TextureRegion backgroundRegion;
     private SpriteBatch batch;
+    private MenuText menuText;
+
 
     public BrainsMenu(Game g){
         game = g;
@@ -28,6 +32,12 @@ public class BrainsMenu implements Screen{
         background = new Texture(Gdx.files.internal("brainsStartScreen.jpg"));
         backgroundRegion = new TextureRegion(background);
         batch = new SpriteBatch();
+        menuText = new MenuText();
+
+        RepeatAction action = Actions.forever(Actions.sequence(Actions.scaleTo(1,1,1), Actions.scaleTo(0,0,1)));
+        menuText.addAction(action);
+
+        uiStage.addActor(menuText);
 
     }
 
@@ -37,16 +47,19 @@ public class BrainsMenu implements Screen{
     }
 
     @Override
-    public void render(float delta) {
+    public void render(float dt) {
 
         if (Gdx.input.isKeyJustPressed(Input.Keys.ENTER))
             game.setScreen(new BrainsLevel(game));
 
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        uiStage.act(dt);
 
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
         batch.draw(backgroundRegion,0,0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         batch.end();
+
+        uiStage.draw();
 
     }
 
---
+++ core/src/com/mygdx3/game/MenuText.java
@@ -0,0 +1,28 @@
+package com.mygdx3.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+
+
+public class MenuText extends Actor{
+
+    private TextureRegion region;
+    private Texture texture;
+
+    public MenuText(){
+        super();
+        texture = new Texture(Gdx.files.internal("pressEnter.png"));
+        region = new TextureRegion(texture);
+        setOrigin(region.getRegionWidth()/2, region.getRegionHeight()/2);
+    }
+
+    @Override
+    public void draw(Batch batch, float parentAlpha) {
+        super.draw(batch, parentAlpha);
+        batch.draw(region, 0, 0, Gdx.graphics.getWidth()/2, region.getRegionHeight()/2,
+                Gdx.graphics.getWidth(), region.getRegionHeight(), getScaleX(), getScaleY(), getRotation());
+    }
+}
